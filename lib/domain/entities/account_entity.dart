360c68cc54e43b5cdf1617f4e01e2a24b8be3966
dionlaranjeira <dionribeiro.rr@gmail.com>
2022-07-21 11:45:05 -0400

    test: Should throw unexpectedError if HttpClient returns 200 with invalid data

--- lib/domain/entities/account_entity.dart
+++ lib/domain/entities/account_entity.dart
@@ -1,9 +1,14 @@
+import '../../data/http/http.dart';
+
 class AccountEntity{
   final String token;
 
   AccountEntity(this.token);
 
-  factory AccountEntity.fromJson(Map json) =>
-      AccountEntity(json['accessToken']);
+  factory AccountEntity.fromJson(Map json){
+    if(!json.containsKey('accessToken')){
+      throw HttpError.invalidData;
+    }
+    return AccountEntity(json['accessToken']);}
 
 }
--- test/data/usecases/remote_authentication.dart
+++ test/data/usecases/remote_authentication.dart
@@ -96,5 +96,16 @@ void main(){
 
   });
 
+  test('Should throw unexpectedError if HttpClient returns 200 with invalid data', () async {
+
+    when(httpClient.request(url: anyNamed('url'), method: anyNamed('method'), body: anyNamed('body')))
+        .thenAnswer((_) async => {'invalid_key': 'invalid_value'});
+
+    final future = sut.auth(params);
+
+    expect(future, throwsA(DomainError.unexpected));
+
+  });
+
 
 }

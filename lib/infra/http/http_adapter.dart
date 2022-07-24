8de4d2f4e8425ebaf93f7161750cafc9b3ec9e2d
dionlaranjeira <dionribeiro.rr@gmail.com>
2022-07-24 19:01:43 -0400

    refactor: moved http_adapter to its own file

---
+++ lib/infra/http/http_adapter.dart
@@ -0,0 +1,36 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+
+import '../../data/http/http.dart';
+import 'package:meta/meta.dart';
+class HttpAdapter implements HttpClient {
+
+  final Client client;
+
+  HttpAdapter(this.client);
+
+  Future<Map> request({
+    @required String url,
+    @required String method,
+    Map body
+  }) async{
+    final headers = {
+      "content-type": "application/json",
+      "accept": "application/json"
+    };
+    final jsonBody = body != null ? jsonEncode(body) : null;
+
+    final response = await client.post(url, headers: headers, body: jsonBody);
+    return _handleResponse(response);
+  }
+
+  Map _handleResponse(Response response) {
+      if(response.statusCode == 200){
+      return response.body.isNotEmpty ? jsonDecode(response.body) : null;
+    }else{
+      return null;
+    }
+  }
+
+}

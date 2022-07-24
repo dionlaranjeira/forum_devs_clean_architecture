0be0acf42c39c11e7a212a04bb88f703d6d41867
dionlaranjeira <dionribeiro.rr@gmail.com>
2022-07-24 18:35:54 -0400

    test: ensure HttpAdapter return null if status code return 200 without data

--- test/infra/http/http_adapter.dart
+++ test/infra/http/http_adapter.dart
@@ -29,7 +29,7 @@ class HttpAdapter implements HttpClient {
       final jsonBody = body != null ? jsonEncode(body) : null;
 
       final response = await client.post(url, headers: headers, body: jsonBody);
-      return jsonDecode(response.body);
+      return response.body.isNotEmpty ? jsonDecode(response.body) : null;
   }
 
 }
@@ -87,6 +87,17 @@ void main(){
       final response = await sut.request(url: url, method: "post");
 
       expect(response,{'any_key':'any_value'});
+
+    });
+
+    test('Should return data if status code 200', () async {
+
+      when(client.post(any, headers: anyNamed('headers')))
+          .thenAnswer((_) async => Response('', 200));
+
+      final response = await sut.request(url: url, method: "post");
+
+      expect(response,null);
 
     });
 

441abaf0ff1d38170d666a3e4f4201b483eee1ee
dionlaranjeira <dionribeiro.rr@gmail.com>
2022-08-24 10:29:05 -0400

    test: ensure StreamLoginPresenter calls validation on password changed

--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -11,6 +11,7 @@ void main(){
   StreamLoginPresenter sut;
   ValidationSpy validation;
   String email;
+  String password;
 
   PostExpectation mockValidationCall(String fiel) =>
       when(validation.validate(field: fiel == null ? anyNamed('field'):fiel, value: anyNamed('value')));
@@ -23,6 +24,7 @@ void main(){
     validation = ValidationSpy();
     sut = StreamLoginPresenter(validation: validation);
     email = faker.internet.email();
+    password = faker.internet.password();
     mockValidation();
   });
 
@@ -51,6 +53,13 @@ void main(){
 
     sut.validateEmail(email);
     sut.validateEmail(email);
+
+  });
+
+  test('Should call Validation with correct password', (){
+
+    sut.validatePassword(password);
+    verify(validation.validate(field: 'password', value: password)).called(1);
 
   });
 

55befd3fcc150834d1649517335509447e2c53b0
dionlaranjeira <dionribeiro.rr@gmail.com>
2022-08-25 12:18:25 -0400

    test: ensure StreamLoginPresenter emits correct events on Unexpected error

--- requeriments/login/login_presenter.md
+++ requeriments/login/login_presenter.md
@@ -15,5 +15,5 @@
 13.✅ Chamar o Authentication com email e senha corretos
 14.✅ Notificar o isLoadingStream como true antes de chamar o Authentication
 15.✅ Notificar o isLoadingStream como false no fim do Authentication
-16. Notificar o mainErrorStream caso o Authentication retorne um DomainError
+16.✅ Notificar o mainErrorStream caso o Authentication retorne um DomainError
 17. Fechar todos os Stream no dispose
--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -147,6 +147,19 @@ void main(){
 
   });
 
+  test('Should emit correct events on Unexpected error', () async {
+    mockAuthenticationError(DomainError.unexpected);
+
+    sut.validateEmail(email);
+    sut.validatePassword(password);
+
+    expectLater(sut.isLoadingStream, emits(false));
+    sut.mainErrorStream.listen(expectAsync1((error) => expect(error, 'Algo errado aconteceu. Tente novamente em breve.')));
+
+    await sut.auth();
+
+  });
+
 
 
 }
